63d6c0b87395a239d6c189c2efe2d5106a4c2cbb
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-10-13 03:46:22 +0600

    complete tip calculator project

---
+++ projects/p05_tip_calculator_web/js/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const calculateBtn = document.querySelector("#calculate_btn");
+const displayTip = document.querySelector("#display_tip");
+const displayBill = document.querySelector("#display_bill");
+const errorMessage = document.querySelector("#error_message");
+
+calculateBtn.addEventListener("click", function () {
+  const billAmountInp = parseFloat(
+    document.querySelector("#bill_amount").value
+  );
+  const tipPercentInp = parseFloat(
+    document.querySelector("#tip_percent").value
+  );
+
+  if (!billAmountInp && !tipPercentInp) {
+    errorMessage.classList.add("error_message");
+    errorMessage.innerText = "Please Enter Amount";
+  } else {
+    const tipAmount = billAmountInp * (tipPercentInp / 100);
+    const totalAmount = billAmountInp + tipAmount;
+
+    displayTip.textContent = `$${tipAmount.toFixed(2)}`;
+    displayBill.textContent = `$${totalAmount.toFixed(2)}`;
+  }
+});
