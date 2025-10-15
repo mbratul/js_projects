2f14f4ae3f6efcb5032e6ba43b2802cfaf2b0310
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-10-15 22:23:53 +0600

    to do application js file update running

--- projects/p11_todo_list_application/js/script.js
+++ projects/p11_todo_list_application/js/script.js
@@ -1,1 +1,24 @@
 "use strict";
+const todoInp = document.querySelector("#todo_inp");
+const addBtn = document.querySelector("#add_btn");
+const listContainer = document.querySelector("#list_container");
+
+addBtn.addEventListener("click", function () {
+  if (!todoInp.value) {
+    alert("Enter Text first");
+  } else {
+    let li = document.createElement("li");
+    li.innerText = todoInp.value;
+    listContainer.appendChild(li);
+  }
+  todoInp.value = "";
+  saveTask();
+});
+
+function saveTask() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
