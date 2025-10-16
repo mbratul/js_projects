7d962af4ac0aa8a09a65521b80816714d18c2eb9
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-10-17 02:50:00 +0600

    salary calculator function added

--- projects/p13_percent_calculator_web/js/script.js
+++ projects/p13_percent_calculator_web/js/script.js
@@ -1,36 +1,85 @@
 "use strict";
-const previous = document.querySelector("#previous");
-const current = document.querySelector("#current");
-const tenure = document.querySelector("#tenure");
-const calculatorBtn = document.querySelector("#calculatorBtn");
-const resetBtn = document.querySelector("#resetBtn");
-const result = document.querySelector("#result");
+{
+  const previous = document.querySelector("#previous");
+  const current = document.querySelector("#current");
+  const tenure = document.querySelector("#tenure");
+  const calculatorBtn = document.querySelector("#calculatorBtn");
+  const resetBtn = document.querySelector("#resetBtn");
+  const result = document.querySelector("#result");
 
-//FD Calculator handler function
-function handlecalculateMaturityAmount() {
-  //get the input value
-  const previousValue = parseFloat(previous.value);
-  const currentValue = parseFloat(current.value);
-  //const tenureValue = parseFloat(tenure.value);
+  //FD Calculator handler function
+  function handlecalculateMaturityAmount() {
+    //get the input value
+    const previousValue = parseFloat(previous.value);
+    const currentValue = parseFloat(current.value);
+    //const tenureValue = parseFloat(tenure.value);
 
-  const absoluteValue = previousValue - currentValue;
-  //percent the calculation
-  const percentValue = (absoluteValue * 100) / previousValue;
-  //Display the Result
-  if (previousValue < currentValue) {
-    result.innerText = `${Math.abs(percentValue.toFixed(2))}% Percent Increase`;
-  } else {
-    result.innerText = `${Math.abs(percentValue.toFixed(2))}% Percent Decrease`;
+    const absoluteValue = previousValue - currentValue;
+    //percent the calculation
+    const percentValue = (absoluteValue * 100) / previousValue;
+    //Display the Result
+    if (previousValue < currentValue) {
+      result.innerText = `${Math.abs(
+        percentValue.toFixed(2)
+      )}% Percent Increase`;
+    } else {
+      result.innerText = `${Math.abs(
+        percentValue.toFixed(2)
+      )}% Percent Decrease`;
+    }
   }
+  //add event listener for button
+
+  calculatorBtn.addEventListener("click", handlecalculateMaturityAmount);
+  // reset button event listener
+
+  resetBtn.addEventListener("click", function () {
+    previous.value = "";
+    current.value = "";
+    //tenure.value = "";
+    result.innerText = "Show Result";
+  });
 }
-//add event listener for button
 
-calculatorBtn.addEventListener("click", handlecalculateMaturityAmount);
-// reset button event listener
+{
+  const currentSalary = document.querySelector("#current_salary");
+  const percentNumber = document.querySelector("#percent_number");
+  const calculatorBtn = document.querySelector("#salary_calculatorBtn");
+  const resetBtn = document.querySelector("#reset_Salary_Btn");
+  const resultNew = document.querySelector("#result_new");
+  const resultTotal = document.querySelector("#result_total");
 
-resetBtn.addEventListener("click", function () {
-  previous.value = "";
-  current.value = "";
-  //tenure.value = "";
-  result.innerText = "Show Result";
-});
+  //FD Calculator handler function
+  function handleSalaryInPercent() {
+    //get the input value
+    const currentSalaryValue = parseFloat(currentSalary.value);
+    const percentNumberValue = parseFloat(percentNumber.value);
+    //const tenureValue = parseFloat(tenure.value);
+
+    const absoluteValue = (currentSalaryValue / 100) * percentNumberValue;
+    //percent the calculation
+    const totalValue = absoluteValue + currentSalaryValue;
+    //Display the Result
+    resultNew.innerText = `New Amount $${Math.abs(absoluteValue.toFixed(2))}`;
+    resultTotal.innerText = `Total Salary $${Math.abs(totalValue.toFixed(2))}`;
+    // if (previousValue < currentValue) {
+    //   result.innerText = `${Math.abs(
+    //     percentValue.toFixed(2)
+    //   )}% Percent Increase`;
+    // } else {
+    //   result.innerText = `${Math.abs(
+    //     percentValue.toFixed(2)
+    //   )}% Percent Decrease`;
+    // }
+  }
+  //add event listener for button
+
+  calculatorBtn.addEventListener("click", handleSalaryInPercent);
+  // reset button event listener
+
+  resetBtn.addEventListener("click", function () {
+    previous.value = "";
+    current.value = "";
+    result.innerText = "Show Result";
+  });
+}
