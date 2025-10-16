f5193a9149f9a9f5c9ef284c2d0d8c799642f1cd
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-10-16 23:23:14 +0600

    weight increase or decrese calculator in percent wise

---
+++ projects/p13_percent_calculator_web/js/script.js
@@ -0,0 +1,34 @@
+"use strict";
+const previous = document.querySelector("#previous");
+const current = document.querySelector("#current");
+const tenure = document.querySelector("#tenure");
+const calculatorBtn = document.querySelector("#calculatorBtn");
+const resetBtn = document.querySelector("#resetBtn");
+const result = document.querySelector("#result");
+
+//FD Calculator handler function
+function handlecalculateMaturityAmount() {
+  //get the input value
+  const previousValue = parseFloat(previous.value);
+  const currentValue = parseFloat(current.value);
+  //const tenureValue = parseFloat(tenure.value);
+
+  const newValue = previousValue - currentValue;
+
+  //percent the calculation
+  const percentValue = (newValue * 100) / previousValue;
+  //Display the Result
+
+  result.innerText = `${percentValue.toFixed(2)} % : Percent `;
+}
+//add event listener for button
+
+calculatorBtn.addEventListener("click", handlecalculateMaturityAmount);
+// reset button event listener
+
+resetBtn.addEventListener("click", function () {
+  priniciple.value = "";
+  interestRate.value = "";
+  //tenure.value = "";
+  result.innerText = "Maturity Amount";
+});
