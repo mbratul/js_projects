655a20829e24c2778b7b8697bf76051079df77ab
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-10-20 23:55:30 +0600

    update project file

--- projects/p14_password_strength_checker/js/script.js
+++ projects/p14_password_strength_checker/js/script.js
@@ -1,1 +1,7 @@
 "use strict";
+const passwordInp = document.querySelector("#password_inp");
+const submitBtn = document.querySelector("#submit_btn");
+const message = document.querySelector("#message");
+const strength = document.querySelector("#strength");
+
+passwordInp.addEventListener("input", function () {});
