d7e15a5c3b50f7c312cbc94075fa32d534b6cb67
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-10-21 00:12:44 +0600

    complete project password strength checker

--- projects/p14_password_strength_checker/js/script.js
+++ projects/p14_password_strength_checker/js/script.js
@@ -4,4 +4,31 @@ const submitBtn = document.querySelector("#submit_btn");
 const message = document.querySelector("#message");
 const strength = document.querySelector("#strength");
 
-passwordInp.addEventListener("input", function () {});
+passwordInp.addEventListener("input", function () {
+  const passwordValue = passwordInp.value;
+  const passwordLength = passwordValue.length;
+
+  let strengthValue = " ";
+
+  if (passwordInp === 0) {
+    strengthValue = "";
+  } else if (passwordLength < 6) {
+    strengthValue = "Weak";
+  } else if (passwordLength < 8) {
+    strengthValue = "Medium";
+  } else {
+    strengthValue = "Strong";
+  }
+  strength.textContent = strengthValue;
+  //Display the message
+  message.style.display = "block";
+});
+
+submitBtn.addEventListener("click", function () {
+  const passwordType = passwordInp.getAttribute("type");
+  if (passwordType === "password") {
+    passwordInp.setAttribute("type", "text");
+  } else {
+    passwordInp.setAttribute("type", "password");
+  }
+});
