8f73cc97447ecbb40026fc7fdd98d2c94950f8f5
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-11-04 20:32:34 +0600

    change bg color project

---
+++ projects/p19_change_BG_color/js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+const body = document.body;
+
+body.addEventListener("click", function () {
+  const randomColor = getRandomColor();
+  body.style.backgroundColor = randomColor;
+});
+
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+
+  return color;
+}
