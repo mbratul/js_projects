3f55581ca0229684e07c76b9fdfff1943a8e87c1
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-11-08 00:00:15 +0600

    rgb color generator

---
+++ projects/p25_RGB_color_generator/js/script.js
@@ -0,0 +1,31 @@
+"use strict";
+const colorBox = document.querySelector("#color_box");
+const redSlider = document.querySelector("#redSlider");
+const greenSlider = document.querySelector("#greenSlider");
+const blueSlider = document.querySelector("#blueSlider");
+const redValueSpan = document.querySelector("#redValue");
+const greenValueSpan = document.querySelector("#greenValue");
+const blueValueSpan = document.querySelector("#blueValue");
+const copy_button = document.querySelector("#copy_button");
+const inputTypeValue = document.querySelector("#input_type_value");
+
+function updateColor() {
+  const redValue = redSlider.value;
+  const greenValue = greenSlider.value;
+  const blueValue = blueSlider.value;
+
+  const rgbColor = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
+  colorBox.style.backgroundColor = rgbColor;
+
+  redValueSpan.textContent = redValue;
+  greenValueSpan.textContent = greenValue;
+  blueValueSpan.textContent = blueValue;
+
+  inputTypeValue.textContent = rgbColor;
+}
+
+redSlider.addEventListener("input", updateColor);
+greenSlider.addEventListener("input", updateColor);
+blueSlider.addEventListener("input", updateColor);
+
+updateColor();
