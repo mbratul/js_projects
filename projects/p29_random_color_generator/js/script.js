9e245b211216d36f0d7c29e67444379f2177126b
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-11-09 22:37:51 +0600

    random color generator

---
+++ projects/p29_random_color_generator/js/script.js
@@ -0,0 +1,62 @@
+"use strict";
+const containerEL = document.querySelector(".container");
+
+for (let i = 0; i < 5; i++) {
+  const colorContainerEL = document.createElement("div");
+  colorContainerEL.classList.add("color_container");
+
+  const colorCodeEL = document.createElement("span");
+  colorCodeEL.classList.add("color_code");
+  colorContainerEL.appendChild(colorCodeEL);
+
+  const btnCopy = document.createElement("button");
+  btnCopy.innerText = "Copy";
+  colorContainerEL.appendChild(btnCopy);
+
+  containerEL.appendChild(colorContainerEL);
+}
+
+function randomColors() {
+  const chars = "0123456789ABCDEF";
+  const colorCodeLength = 6;
+  let colorCode = "";
+
+  for (let i = 0; i < colorCodeLength; i++) {
+    const randomNums = Math.floor(Math.random() * chars.length);
+    colorCode += chars.substring(randomNums, randomNums + 1);
+  }
+  return colorCode;
+}
+
+const colorContainerEls = document.querySelectorAll(".color_container");
+function generateColor() {
+  for (let i = 0; i < colorContainerEls.length; i++) {
+    const colorContainerEl = colorContainerEls[i];
+    const newColorCode = randomColors();
+    const colorCodeEl = document.querySelector(".color_code");
+
+    colorContainerEl.style.backgroundColor = "#" + newColorCode;
+    colorCodeEl.innerText = "#" + newColorCode;
+  }
+}
+generateColor();
+
+colorContainerEls.forEach((colorContainerEL) => {
+  const copyButtonEl = colorContainerEL.querySelector("button");
+  const colorCodeEl = colorContainerEL.querySelector(".color_code");
+
+  copyButtonEl.addEventListener("click", function () {
+    const colorCode = colorCodeEl.innerText;
+    copyToClipboard(colorCode);
+  });
+});
+function copyToClipboard(text) {
+  navigator.clipboard
+    .writeText(text)
+    .then(() => {
+      alert("Copy to Clipboard : " + text);
+    })
+    .catch((error) => {
+      console.error("failed to copy clipboard", error);
+    });
+}
