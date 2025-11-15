39543cf70574e53b910a9795bd7ae9b9ba7ebabb
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-11-15 22:30:12 +0600

    update js file

--- projects/p36_javascript_array_methods/js/script.js
+++ projects/p36_javascript_array_methods/js/script.js
@@ -1,31 +1,66 @@
 "use strict";
-const numbers = [20, 15, 45, 56, 45, 75];
+{
+  /**
+   * filter number and sum of total value
+   * @param {number}
+   * @returns {number}
+   */
+  const numbers = [20, 15, 45, 56, 45, 75];
 
-const total = numbers
-  .filter(function (number) {
-    return number >= 50;
-  })
-  .reduce(function (total, number) {
-    return total + number;
-  }, 0);
-console.log(total);
+  const total = numbers
+    .filter(function (number) {
+      return number >= 50;
+    })
+    .reduce(function (total, number) {
+      return total + number;
+    }, 0);
+  console.log(total);
 
-const arr = [
-  "laurence",
-  "sam",
-  "michel",
-  ,
-  ,
-  "milton",
-  ,
-  ,
-  "laurence",
-  "sam",
-  "michel",
-  ,
-  ,
-  "milton",
-  ,
-  ,
-];
-console.log(arr);
+  /**
+   * clean up array and filter the array with Set operator
+   */
+
+  const arr = [
+    "laurence",
+    "sam",
+    "michel",
+    null,
+    ,
+    "milton",
+    0,
+    ,
+    "laurence",
+    "sam",
+    "michel",
+    false,
+    ,
+    "milton",
+    undefined,
+    ,
+  ];
+  console.log(arr);
+
+  const arr1 = [...new Set(arr)];
+  console.log(arr1);
+
+  const arr2 = arr.filter(Boolean);
+  console.log(arr2);
+
+  const arr3 = [...new Set(arr.filter(Boolean))];
+  console.log(arr3);
+}
+{
+  /**
+   * how to loop through an array
+   */
+  const arr = [
+    "laurence",
+    "sam",
+    "michel",
+    "milton",
+    "laurence",
+    "sam",
+    "michel",
+    "milton",
+  ];
+}
