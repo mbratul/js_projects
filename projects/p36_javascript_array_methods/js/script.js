4df78ef688f4c94f023245b95b278779ff326c13
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-11-16 21:43:04 +0600

    array method learning

--- projects/p36_javascript_array_methods/js/script.js
+++ projects/p36_javascript_array_methods/js/script.js
@@ -14,7 +14,7 @@
     .reduce(function (total, number) {
       return total + number;
     }, 0);
-  console.log(total);
+  //console.log(total);
 
   /**
    * clean up array and filter the array with Set operator
@@ -38,16 +38,16 @@
     undefined,
     ,
   ];
-  console.log(arr);
+  //console.log(arr);
 
   const arr1 = [...new Set(arr)];
-  console.log(arr1);
+  //console.log(arr1);
 
   const arr2 = arr.filter(Boolean);
-  console.log(arr2);
+  //console.log(arr2);
 
   const arr3 = [...new Set(arr.filter(Boolean))];
-  console.log(arr3);
+  // console.log(arr3);
 }
 {
   /**
@@ -63,4 +63,97 @@
     "michel",
     "milton",
   ];
+  /**
+   * for loop in an array
+   */
+  // console.log("***for loop***");
+  for (let i = 0; i < arr.length; i++) {
+    // console.log(arr[i]);
+  }
+
+  /**
+   * while loop in an array
+   */
+  // console.log("***while loop***");
+  let i = 0;
+  while (i < arr.length) {
+    // console.log(arr[i]);
+    i++;
+  }
+  /**
+   * forEach array method
+   */
+  //console.log("***forEach method***");
+  arr.forEach((item, index, array) => {
+    //console.log(item, index);
+    //console.log(array.length);
+  });
+  /**
+   * map array method
+   */
+  //console.log("***map method***");
+  const mapArray = arr.map((item) => {
+    const person = `Dear ${item}`;
+    //console.log(person);
+    return person;
+  });
+  //console.log(mapArray);
+  // console.log(...mapArray);
+  /**
+   * filter array method
+   */
+  //console.log("***filter method***");
+  const filterArray = arr.filter((item) => {
+    const person = item.includes("sam");
+    const length = item.length;
+    //console.log(length);
+    return person;
+  });
+  // console.log(filterArray);
+}
+{
+  /**
+   * Array updating Adding and Removing array items
+   */
+  const arr = ["laurence", "sam", "michel", "milton"];
+  arr.push("end push");
+  // console.log(arr);
+  const removeFirstItem = arr.shift();
+  // console.log(removeFirstItem);
+  // console.log(arr);
+  const removeLastItem = arr.pop();
+  // console.log(removeLastItem);
+  // console.log(arr);
+  arr.unshift("start item");
+  // console.log(arr);
+  const str1 = arr.toString();
+  // console.log(str1);
+  const str2 = arr.join("-");
+  // console.log(str2);
+}
+{
+  /**
+   * JS Map method
+   */
+  const arr = ["milton", 4, 457, false, 0, "", "test"];
+  console.log(arr);
+
+  const falseValue = arr.filter((item) => {
+    return !item;
+  });
+  console.log(falseValue);
+  const arr2 = [4, 45, 75, 65, 48];
+  const arr3 = arr2.map((item) => {
+    return item * 2;
+  });
+  console.log(arr3);
+
+  const arrayObj = [
+    { first: "milton", last: "jomadder" },
+    { first: "saker", last: "talukder" },
+  ];
+  const arr7 = arrayObj.map(({ first, last }) => {
+    return { fulname: `${first}, ${last}` };
+  });
+  console.log(arr7);
 }
